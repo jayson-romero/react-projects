99a22a46b2454e7f6cbf6d2d43048e066f88f657
ItonRomero <jaysonromero1218@gmail.com>
2023-04-17 22:56:18 +0800

    added

--- 01-birthday-reminder/my-version/src/App.jsx
+++ 01-birthday-reminder/my-version/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <main className="App">
       <div className='container w-[450px] px-[1.5rem] py-[2rem] bg-primary'>
-        <h1 className={`${style.heading} mb-[20px]`}>{person.length} Birthday{`${person.length === 0 ? "": "s"}`} today</h1>
+        <h1 className={`${style.heading} mb-[20px]`}>{person.length} Birthday{`${person.length === 0 ? "": "s"}`} today test</h1>
         <List person={person}/>
         <button className='btn' onClick={() => setPerson([])}>
             Clear
